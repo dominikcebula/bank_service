0d07c138976b22e87a171bb056536a06dae0a04b
Dominik Cebula <dominikcebula@gmail.com>
2019-02-05 00:53:58 +0100

    simple refactorings

--- src/main/java/com/dominikcebula/bank/service/rest/actions/ErrorHandlingAction.java
+++ src/main/java/com/dominikcebula/bank/service/rest/actions/ErrorHandlingAction.java
@@ -2,14 +2,18 @@ package com.dominikcebula.bank.service.rest.actions;
 
 import com.dominikcebula.bank.service.rest.ds.response.ErrorResponse;
 import com.dominikcebula.bank.service.rest.json.GsonFactory;
+import com.google.gson.Gson;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 
 public class ErrorHandlingAction implements Route {
+
+    private Gson gson = new GsonFactory().create();
+
     @Override
     public Object handle(Request request, Response response) {
-        return new GsonFactory().create().toJson(
+        return gson.toJson(
                 new ErrorResponse("Error occurred while processing request.")
         );
     }
--- src/main/java/com/dominikcebula/bank/service/rest/validator/exception/ValidatorExceptionHandler.java
+++ src/main/java/com/dominikcebula/bank/service/rest/validator/exception/ValidatorExceptionHandler.java
@@ -2,15 +2,19 @@ package com.dominikcebula.bank.service.rest.validator.exception;
 
 import com.dominikcebula.bank.service.rest.ds.response.ErrorResponse;
 import com.dominikcebula.bank.service.rest.json.GsonFactory;
+import com.google.gson.Gson;
 import spark.ExceptionHandler;
 import spark.Request;
 import spark.Response;
 
 public class ValidatorExceptionHandler implements ExceptionHandler<ValidatorException> {
+
+    private Gson gson = new GsonFactory().create();
+
     @Override
     public void handle(ValidatorException exception, Request request, Response response) {
         response.body(
-                new GsonFactory().create().toJson(
+                gson.toJson(
                         new ErrorResponse(exception.getMessage())
                 )
         );
