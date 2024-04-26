b1d8649c0b6362d475396b20009afabdc3f49c98
Dominik Cebula <dominikcebula@gmail.com>
2024-04-26 22:32:35 +0200

    Upgrading to Java 21

--- src/main/java/com/dominikcebula/bank/service/rest/validator/validators/JavaBeanValidator.java
+++ src/main/java/com/dominikcebula/bank/service/rest/validator/validators/JavaBeanValidator.java
@@ -3,12 +3,12 @@ package com.dominikcebula.bank.service.rest.validator.validators;
 import com.dominikcebula.bank.service.rest.validator.Validator;
 import com.dominikcebula.bank.service.rest.validator.exception.ValidatorException;
 
-import javax.validation.ConstraintViolation;
-import javax.validation.Validation;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
 import java.util.Set;
 
 public class JavaBeanValidator<T> extends Validator<T> {
-    private final javax.validation.Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+    private final jakarta.validation.Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
 
     @Override
     public void validate(T value) throws ValidatorException {
