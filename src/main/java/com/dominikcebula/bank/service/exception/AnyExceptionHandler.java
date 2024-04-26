e466ccc86829f8a9781cb64629aed0dc8f45f51a
Dominik Cebula <dominikcebula@gmail.com>
2024-04-26 22:56:53 +0200

    Upgrading to Java 21

---
+++ src/main/java/com/dominikcebula/bank/service/exception/AnyExceptionHandler.java
@@ -0,0 +1,40 @@
+package com.dominikcebula.bank.service.exception;
+
+import com.dominikcebula.bank.service.dto.ApiCode;
+import com.dominikcebula.bank.service.dto.ApiErrorResponse;
+import com.dominikcebula.bank.service.dto.ModelApiResponse;
+import com.dominikcebula.bank.service.logging.Loggers;
+import com.google.gson.Gson;
+import com.google.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import spark.ExceptionHandler;
+import spark.Request;
+import spark.Response;
+
+public class AnyExceptionHandler implements ExceptionHandler<Exception> {
+    private final Logger logger = LoggerFactory.getLogger(Loggers.REST);
+
+    private final Gson gson;
+
+    @Inject
+    @SuppressWarnings("unused")
+    public AnyExceptionHandler(Gson gson) {
+        this.gson = gson;
+    }
+
+    @Override
+    public void handle(Exception exception, Request request, Response response) {
+        logger.error(exception.getMessage(), exception);
+
+        response.status(500);
+
+        response.body(
+                gson.toJson(
+                        new ApiErrorResponse()
+                                .status(new ModelApiResponse().code(ApiCode.FAILED))
+                                .message("Unexpected error occurred while processing request.")
+                )
+        );
+    }
+}
--- src/main/java/com/dominikcebula/bank/service/rest/server/RestServer.java
+++ src/main/java/com/dominikcebula/bank/service/rest/server/RestServer.java
@@ -1,11 +1,13 @@
 package com.dominikcebula.bank.service.rest.server;
 
 import com.dominikcebula.bank.service.configuration.Configuration;
+import com.dominikcebula.bank.service.exception.AnyExceptionHandler;
 import com.dominikcebula.bank.service.exception.ReportableException;
 import com.dominikcebula.bank.service.exception.ReportableExceptionHandler;
 import com.dominikcebula.bank.service.rest.actions.*;
 import com.dominikcebula.bank.service.rest.filters.ResponseFilter;
 import com.google.inject.Inject;
+import lombok.RequiredArgsConstructor;
 import spark.Spark;
 
 import static com.dominikcebula.bank.service.rest.actions.CreateAccountRestAction.ACCOUNTS_CREATE_URI;
@@ -13,32 +15,18 @@ import static com.dominikcebula.bank.service.rest.actions.IndexRestAction.INDEX_
 import static com.dominikcebula.bank.service.rest.actions.ListAccountsRestAction.ACCOUNT_LIST_URI;
 import static com.dominikcebula.bank.service.rest.actions.TransferMoneyRestAction.TRANSFER_URI;
 
+@RequiredArgsConstructor(onConstructor = @__({@Inject}))
 public class RestServer {
 
     private final Configuration configuration;
     private final ResponseFilter responseFilter;
     private final ErrorHandlingRestAction errorHandlingRestAction;
     private final ReportableExceptionHandler reportableExceptionHandler;
+    private final AnyExceptionHandler anyExceptionHandler;
     private final IndexRestAction indexRestAction;
     private final CreateAccountRestAction createAccountRestAction;
     private final TransferMoneyRestAction transferMoneyAction;
     private final ListAccountsRestAction listAccountsRestAction;
-
-    @Inject
-    public RestServer(Configuration configuration, ResponseFilter responseFilter,
-                      ErrorHandlingRestAction errorHandlingRestAction, ReportableExceptionHandler reportableExceptionHandler,
-                      IndexRestAction indexRestAction, CreateAccountRestAction createAccountRestAction,
-                      TransferMoneyRestAction transferMoneyAction, ListAccountsRestAction listAccountsRestAction) {
-        this.configuration = configuration;
-        this.responseFilter = responseFilter;
-        this.errorHandlingRestAction = errorHandlingRestAction;
-        this.reportableExceptionHandler = reportableExceptionHandler;
-        this.indexRestAction = indexRestAction;
-        this.createAccountRestAction = createAccountRestAction;
-        this.transferMoneyAction = transferMoneyAction;
-        this.listAccountsRestAction = listAccountsRestAction;
-    }
-
 
     public void start() {
         Spark.ipAddress(configuration.getHost());
@@ -50,6 +38,7 @@ public class RestServer {
         Spark.notFound(errorHandlingRestAction);
         Spark.internalServerError(errorHandlingRestAction);
         Spark.exception(ReportableException.class, reportableExceptionHandler);
+        Spark.exception(Exception.class, anyExceptionHandler);
 
         Spark.get(INDEX_ACTION_URI, indexRestAction);
         Spark.post(ACCOUNTS_CREATE_URI, createAccountRestAction);
